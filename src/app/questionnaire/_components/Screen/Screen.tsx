e9e0be106bb1d20055350fe074244656617c9b1d
Ruslan Sedziukh <ruslan.sedziukh@gmail.com>
2025-03-20 19:43:53 +0200

    Add header elements

--- src/app/questionnaire/_components/Screen/Screen.tsx
+++ src/app/questionnaire/_components/Screen/Screen.tsx
@@ -1,6 +1,7 @@
 import { QuestionnaireData } from '@/redux/questionnaireSlice'
 import { Question } from '@/types/questionnaire'
 import { getTextWithDynamicValues } from '@/utils/getTextWithDynamicValues'
+import Image from 'next/image'
 
 type Props = {
   question: Question
@@ -15,6 +16,18 @@ const Screen = ({
 }: Props) => {
   return (
     <div className="flex items-center font-open-sans flex-col gap-5 bg-[#FFF0F0] min-h-lvh p-4 min-w-fit">
+      <header className="relative flex justify-center w-full max-w-5xl">
+        <Image
+          className="absolute left-0"
+          src="/chevron.svg"
+          width={24}
+          height={24}
+          alt="chevron"
+        />
+
+        <Image src="/logo_black.svg" width={24} height={24} alt="logo" />
+      </header>
+
       <h1 className="inline font-bold text-2xl leading-7 w-[330px]">
         {getTextWithDynamicValues(question.text, questionnaireData)}
       </h1>
