430d37c991aa24500ec06a830254e5c6724a87ba
Ruslan Sedziukh <ruslan.sedziukh@gmail.com>
2025-03-25 16:52:16 +0200

    Install and config prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
